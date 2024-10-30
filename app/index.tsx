9e049ba10fd0551872cc28a02bf3fc50530190d0
joshlynch2001 <joshlynch2001@gmail.com>
2024-10-30 04:05:53 +0000

    making progress

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,20 @@
 import { Text, View } from "react-native";
 import "./styles/home.css";
+import { Image } from 'react-native';
+import Josh from './styles/josh.png';
+
 
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "blue",
-      }}
-    >
-      <Text>home screen</Text>
+    <View>
+      <div className="tasks">
+        <h1>Tasks</h1>
+
+      </div>
+      <div className="characterContainer">
+        <Image source={Josh} style={{ width: 200, height: 200 }} />
+        <div className="characterStats">my starts will be here</div>
+      </div>
     </View>
   );
 }
